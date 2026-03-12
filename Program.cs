7c2abc3d827d858c2be9e759b50f840395f673d4
morganssch <coltm576@gmail.com>
2026-03-12 09:26:00 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Methods_MORGAN_JOSEPH
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            IntegerMultiplier();
+            introduction();
+            goodbye();
+        }
+        static void IntegerMultiplier()
+        {
+           int num1 = 5;
+              int num2 = 10;
+
+               int result = num1 * num2;
+            Console.WriteLine($" 5 * 10 = {result}");
+        }
+        static void introduction()
+        {
+         
+            Console.WriteLine("What is your name?");
+                string name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}! It is nice to meet you!");
+
+        }
+        static void goodbye()
+        {
+            Console.WriteLine("Have a fantastic day, wigga!");
+        }
+    }
+}
